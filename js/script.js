b56925854adc8c05fcebfb4d795f4fe223385f4e
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-08-10 16:31:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-const power = function(base, exponent){
-	let result = 1;
-	for (let count = 0; count < exponent; count++){
-		result *= base;
-	}
-	return result;
-}
-console.log(power(2,6))
+function showMessage() {
+	console.log("Hello, world!");
+  }
+  
+  let count = 0;
+  
+  while (count <= 5) {
+	showMessage(); // Calling the function
+	count++;
+  }
+  
