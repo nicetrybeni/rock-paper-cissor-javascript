11937f93af048bb919664d7bc18230bab23d7f78
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-08-11 02:56:34 +0800

    function and arrow func
    function square(x){
        return x * x;
    }
    console.log(square(4));
    const square1 = (a) => { return a * a;};
    console.log(square1(3));
    const square2 = b => b * b;
    console.log(square2(2));

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-function showMessage() {
-	console.log("Hello, world!");
-  }
-  
-  let count = 0;
-  
-  while (count <= 5) {
-	showMessage(); // Calling the function
-	count++;
-  }
-  
+function square(x){
+    return x * x;
+}
+console.log(square(4));
+const square1 = (a) => { return a * a;};
+console.log(square1(3));
+const square2 = b => b * b;
+console.log(square2(2));
