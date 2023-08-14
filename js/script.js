2171cfe00218138e11315ed2382e09a40396f70d
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-08-14 21:14:41 +0800

    Advancen printFarmInventory

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-function printFarmInventory(cows, chickens){
-    let cowString = String(cows);
-    while (cowString.length < 3){
-        cowString = "0"+ cowString;
-    }
-    console.log(`${cowString} Cows`);
-    let chickensString = String(chickens);
-    while (chickensString.length <3){
-        chickensString = "0"+ chickensString;
-    }
-    console.log(`${chickensString} Chickens`);
+function printZeroPaddWithLabel(number, label){
+    let numberString = String(number);
+    while (numberString.length < 3){
+        numberString = "0" + numberString;
 }
-printFarmInventory(7,11);
+console.log(`${numberString} ${label}`);
+}
+
+function printFarmInventory(cows, chickens, pigs){
+    printZeroPaddWithLabel(cows, "Cows");
+    printZeroPaddWithLabel(chickens, "Chicken");
+    printZeroPaddWithLabel(pigs, "Pigs");
+}
+printFarmInventory(7, 11, 3);
