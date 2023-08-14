d07be3256197f71eed777555344a8054c06ef7da
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-08-14 21:17:15 +0800

    Professional way printFarmInventory

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-function printZeroPaddWithLabel(number, label){
-    let numberString = String(number);
-    while (numberString.length < 3){
-        numberString = "0" + numberString;
-}
-console.log(`${numberString} ${label}`);
-}
-
-function printFarmInventory(cows, chickens, pigs){
-    printZeroPaddWithLabel(cows, "Cows");
-    printZeroPaddWithLabel(chickens, "Chicken");
-    printZeroPaddWithLabel(pigs, "Pigs");
-}
-printFarmInventory(7, 11, 3);
+function zeroPad(number, width) {
+    let string = String(number);
+    while (string.length < width) {
+    string = "0" + string;
+    }
+    return string;
+    }
+    function printFarmInventory(cows, chickens, pigs) {
+    console.log(`${zeroPad(cows, 3)} Cows`);
+    console.log(`${zeroPad(chickens, 3)} Chickens`);
+    console.log(`${zeroPad(pigs, 3)} Pigs`);
+    }
+    printFarmInventory(7, 16, 3);
+    
