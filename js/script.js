a10e74f3b214385548f81c3ac04e00485d83baa6
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-08-14 20:50:32 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-function power1(base, exponent){
-    if(exponent == 0){
-        return 1;
-    }else{
-        return base * power1(base, exponent - 1); 
-    }
+function smolest (a, b) {
+    if (a < b) return a;
+    else return b;
 }
-console.log(power1(2,3))
-
-function power2(base, exponent){
-    let result = 1;
-    for (let count = 0; count < exponent; count++){
-        result *= base;
-    }
-    return result;
-}
-console.log(power2(2,3));
